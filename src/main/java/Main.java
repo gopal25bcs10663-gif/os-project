623c6db997cdbc0cfc8bcc25ab7a3f0d7ae8fcee
GOPAL GUPTA <gopal.25bcs10663@sst.scaler.com>
2026-06-19 16:44:51 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,27 +27,34 @@ public class Main {
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
-            if (input.equals("exit")) {
+            if (input.equals("exit 0")) {
                 break;
-            } else if (input.startsWith("echo ")) {
+            }
+
+            else if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
-            } else if (input.startsWith("type ")) {
+            }
+
+            else if (input.startsWith("type ")) {
                 String cmd = input.substring(5);
 
-                if (cmd.equals("echo") || cmd.equals("exit") || cmd.equals("type")) {
+                if (cmd.equals("echo") ||
+                    cmd.equals("exit") ||
+                    cmd.equals("type")) {
+
                     System.out.println(cmd + " is a shell builtin");
-                
                 } else {
                     String executablePath = findExecutable(cmd);
 
                     if (executablePath != null) {
                         System.out.println(cmd + " is " + executablePath);
                     } else {
-                    System.out.println(cmd + ": not found");
+                        System.out.println(cmd + ": not found");
                     }
                 }
+            }
 
-            } else {
+            else {
                 System.out.println(input + ": command not found");
             }
         }
