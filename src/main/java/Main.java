715efa105620d307699d968e809c71bc0873c1e5
GOPAL GUPTA <gopal.25bcs10663@sst.scaler.com>
2026-06-19 16:34:31 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,10 +8,13 @@ public class Main {
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
-             if (input.equals("exit")) {
+            if (input.equals("exit")) {
                 break;
+            } else if (input.startsWith("echo ")) {
+                System.out.println(input.substring(5));
+            } else {
+                System.out.println(input + ": command not found");
             }
-            System.out.println(input + ": command not found");
         }
     }
 }
