1ba0d92e22024e88311ef6ae66e506273e18d338
GOPAL GUPTA <gopal.25bcs10663@sst.scaler.com>
2026-06-19 16:38:20 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,15 @@ public class Main {
                 break;
             } else if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
+            } else if (input.startsWith("type ")) {
+                String cmd = input.substring(5);
+
+                if (cmd.equals("echo") || cmd.equals("exit") || cmd.equals("type")) {
+                    System.out.println(cmd + " is a shell builtin");
+                } else {
+                    System.out.println(cmd + ": not found");
+                }
+
             } else {
                 System.out.println(input + ": command not found");
             }
