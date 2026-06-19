6ec4b87ac9576353706b5f178cfdf0b0993ea11b
GOPAL GUPTA <gopal.25bcs10663@sst.scaler.com>
2026-06-19 16:41:07 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
+    private static String findExecutable(String command) {
+        String pathEnv = System.getenv("PATH");
+
+        if (pathEnv == null) {
+            return null;
+        }
+
+        String[] directories = pathEnv.split(File.pathSeparator);
+
+        for (String dir : directories) {
+            File file = new File(dir, command);
+
+            if (file.exists() && file.isFile() && file.canExecute()) {
+                return file.getAbsolutePath();
+            }
+        }
+
+        return null;
+    }
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
@@ -17,6 +36,14 @@ public class Main {
 
                 if (cmd.equals("echo") || cmd.equals("exit") || cmd.equals("type")) {
                     System.out.println(cmd + " is a shell builtin");
+                } else {
+                    String executablePath = findExecutable(cmd);
+
+                    if (executablePath != null) {
+                        System.out.println(cmd + " is " + executablePath);
+                    } else {
+                        System.out.println(cmd + ": not found");
+                    }
                 } else {
                     System.out.println(cmd + ": not found");
                 }
